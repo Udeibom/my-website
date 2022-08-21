576168810ce42593a04a9d3cc4566eb012ad67c4
Udeibom <chimmeremkc@gmail.com>
2022-08-21 14:09:48 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,20 @@
+let nextIcon = document.getElementById("next");
+
+function nextOver() {
+  nextIcon.style.display = "inline-block";
+}
+
+function nextOver2() {
+  nextIcon.style.display = "none";
+}
+
+let personal = document.getElementById("personal");
+
+function hidePersonal() {
+  if (personal.style.display = "none") {
+    hideDiv() === true;
+  }
+  function hideDiv() {
+    personal.style.display = "flex";
+  }
+}
